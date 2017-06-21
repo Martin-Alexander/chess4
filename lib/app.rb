0536d35f14536aa37379e512b71f879ae879059c
Martin Giannakopoulos <martingianna@gmail.com>
2017-06-20 21:09:38 -0400

    added GameState model to app.rb and set up 'tests' folder for rake tasks.

--- lib/app.rb
+++ lib/app.rb
@@ -5,4 +5,6 @@ require_relative "models/board"
 require_relative "models/move"
 require_relative "models/game_state"
 
+require_relative "modules/legal_moves"
+
 MODEL_VALIDATIONS = true
--- test/test.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../lib/app'
-
-class RunTests
-  def self.run
-    board = Board.new
-    board.to_s
-  end
-end
