6a76cae72dab6a03107932c04ec2035137718ea5
Martin Giannakopoulos <martingianna@gmail.com>
2017-06-23 17:29:43 -0400

    created spec for LegalMoves (currently unused and no plans for TDD with this one)

---
+++ spec/05_legal_moves_spec.rb
@@ -0,0 +1,12 @@
+# require_relative '../lib/app'
+
+# describe "game state" do
+#   context "find_player_pieces" do
+#     let(:board) { Board.new }
+#     let(:game_state) { GameState.new(board, { wk: true, wq: true, bk: true, bq: true }, :a1, true) }
+#     let(:move) { Move.new(:e2, :e4, false, false) }
+#     it "should return an array of all pieces belonging to the turnplayer" do
+#       game_state.run
+#     end
+#   end
+# end
